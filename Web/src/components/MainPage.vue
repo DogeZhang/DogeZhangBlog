5acadeba307d6e794752ae1e2b61508310c83613
DogeZhang <zhangyz86@hotmail.com>
2020-10-21 00:12:14 +0800

    添加滚动防抖

--- Web/src/components/MainPage.vue
+++ Web/src/components/MainPage.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div ref="mainPanel">
     <div @click="movementRight()" class="click-panel">right</div>
     <div @click="movementLeft()" class="click-panel">left</div>
     <div @click="setOne()" class="click-panel">one</div>
     <div @click="setTwo()" class="click-panel">two</div>
     <div @click="setThree()" class="click-panel">three</div>
+    {{msg}}
     <!-- 宽屏设备展示 -->
     <el-row class="hidden-md-and-down main-container">
       <div style="position: absolute; left: 10%">
@@ -48,13 +49,14 @@
 
 <script>
 export default {
-  name: 'Template',
+  name: 'MainPage',
   mounted () {
+    var mainPanel = this.$refs['mainPanel']
     window.onmousewheel = document.onmousewheel = this.handleScroll
-    window.addEventListener('mousewheel', this.handleScroll, false)
+    mainPanel.addEventListener('mousewheel', this.handleScroll, false)
     // window.addEventListener('DOMMouseScroll', this.handleScroll, false)
-    window.addEventListener('touchstart', this.handleScroll, false)
-    window.addEventListener('touchmove', this.handleScroll, false)
+    mainPanel.addEventListener('touchstart', this.handleScroll, false)
+    mainPanel.addEventListener('touchmove', this.handleScroll, false)
   },
   data () {
     return {
@@ -65,7 +67,9 @@ export default {
       testShow: true,
       movement: [],
       movementsytle: {},
-      textpain: ['one', 'two', 'three']
+      textpain: ['one', 'two', 'three'],
+      textpainIndex: 0,
+      isAnimate: false
     }
   },
   methods: {
@@ -78,51 +82,64 @@ export default {
       console.log('movementLeft')
     },
     setOne () {
-      this.showTextpain = this.textpain[0]
+      !this.isAnimate && this.showCase(0)
     },
     setTwo () {
-      this.showTextpain = this.textpain[1]
+      !this.isAnimate && this.showCase(1)
     },
     setThree () {
-      this.showTextpain = this.textpain[2]
+      !this.isAnimate && this.showCase(2)
+    },
+    showCase (index) {
+      this.isAnimate = true
+      console.log('showCase' + this.isAnimate)
+      this.showTextpain = this.textpain[index]
+      this.textpainIndex = index
+      setTimeout(() => {
+        this._resetAnimating()
+      }, 200)
     },
     handleScroll: function (event) {
-      switch (event.type) {
-        case 'mousewheel':
-          this.msg = 'mousewheel'
-          if (event.wheelDelta > 0) {
-            this.showTextpain = false
-          } else {
-            this.showTextpain = true
-          }
-          break
-        case 'touchstart':
-          this.msg = 'touchstart'
-          this.startX = event.touches[0].pageX
-          this.startY = event.touches[0].pageY
-          break
-        case 'touchmove':
-          this.msg = 'touchmove'
-          var endX = event.touches[0].pageX
-          var endY = event.touches[0].pageY
-          var distanceX = this.startX - endX
-          var distanceY = this.startY - endY
-          if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX > 0) {
-            // this.msg = '往右滑动'
-            this.movementShow = false
-          } else if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX < 0) {
-            // this.msg = '往左滑动'
-            this.movementShow = true
-          } else if (Math.abs(distanceY) > Math.abs(distanceX) && distanceY > 0) {
-            // this.msg = '往上滑动'
-            this.showTextpain = false
-          } else if (Math.abs(distanceY) > Math.abs(distanceX) && distanceY < 0) {
-            // this.msg = '往下滑动'
-            this.showTextpain = true
-          } else {
-            // this.msg = '点击未滑动'
-          }
-          break
+      // setTimeout(t.isAnimate = false, 300)
+      if (!this.isAnimate) {
+        switch (event.type) {
+          case 'mousewheel':
+            this.msg = 'mousewheel'
+            if (event.wheelDelta) {
+              event.wheelDelta > 0 && this.preShowcase()
+              event.wheelDelta < 0 && this.nextShowcase()
+            }
+            break
+          case 'touchstart':
+            this.msg = 'touchstart'
+            this.startX = event.touches[0].pageX
+            this.startY = event.touches[0].pageY
+            break
+          case 'touchmove':
+            this.msg = 'touchmove'
+            var endX = event.touches[0].pageX
+            var endY = event.touches[0].pageY
+            var distanceX = this.startX - endX
+            var distanceY = this.startY - endY
+            if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX > 0) {
+              // this.msg = '往右滑动'
+              // this.movementShow = false
+            } else if (Math.abs(distanceX) > Math.abs(distanceY) && distanceX < 0) {
+              // this.msg = '往左滑动'
+              // this.movementShow = true
+            } else if (Math.abs(distanceY) > Math.abs(distanceX) && distanceY > 0) {
+              this.nextShowcase()
+              // this.showTextpain = false
+            } else if (Math.abs(distanceY) > Math.abs(distanceX) && distanceY < 0) {
+              this.msg = '往下滑动'
+              // this.showTextpain = true
+              this.preShowcase()
+            } else {
+              // this.msg = '点击未滑动'
+            }
+            break
+        }
+        // end switch
       }
       // console.log(event)
       // var str = ''
@@ -130,6 +147,18 @@ export default {
       //   str += item + ':' + event[item] + '\n'
       // }
       // this.msg = str
+    },
+    preShowcase () {
+      this.textpainIndex > 0 && this.showCase(this.textpainIndex - 1)
+    },
+    nextShowcase () {
+      this.textpainIndex < this.textpain.length - 1 && this.showCase(this.textpainIndex + 1)
+    },
+    _resetAnimating () {
+      setTimeout(() => {
+        this.isAnimate = false
+        console.log('setTimeout: ' + this.isAnimate)
+      }, 500)
     }
   }
 }
